a863f8db177511ff08bef1987dc32a75ce416885
MarcBaleiron <mbaleand@myuax.com>
2024-10-13 12:14:18 +0200

    Clase Persona: Persona.cpp

--- Estudiante.h
+++ Estudiante.h
@@ -1,1 +1,17 @@
+#ifndef ESTUDIANTE_H
+#define ESTUDIANTE_H
 
+#include <string>
+#include "Persona.h"
+
+class Estudiante : public Persona
+{
+    std::string grado;
+
+public:
+    void setGrado (const std::string &grado);
+    std::string getGrado();
+    void mostrarDetalles ();
+};
+
+#endif
