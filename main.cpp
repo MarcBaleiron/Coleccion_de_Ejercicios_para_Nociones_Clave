6c02075cd793cc20c5a6343f9944fd3fc102019a
MarcBaleiron <mbaleand@myuax.com>
2024-10-13 10:32:56 +0200

    Función de suma

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int suma (int a, int b)
+{
+    return a + b;
+}
+
 int main() {
-    std::cout << "Hola Mundo" << std::endl;
+    std::cout << "La suma es: " << suma (5, 3) << std::endl;
     return 0;
 }
