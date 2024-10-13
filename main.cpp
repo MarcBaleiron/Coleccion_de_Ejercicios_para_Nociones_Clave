572cdd4b57117adf28c678dd59b0ff1070389100
MarcBaleiron <mbaleand@myuax.com>
2024-10-13 10:40:36 +0200

    Función de suma con Variable global

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@ int suma (int a, int b)
     return a + b;
 }
 
+int g = 20;
+
 int main() {
-    std::cout << "La suma es: " << suma (5, 3) << std::endl;
+    std::cout << "valor de g antes de la función suma: " << g << std::endl;
+    std::cout << "La suma es: " << suma (5, g) << std::endl;
+    std::cout << "Valor de g después de la función suma: " << g << std::endl;
     return 0;
 }
