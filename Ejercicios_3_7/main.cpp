3e45be1156e765ecf07467b57527f4600a9e23b2
MarcBaleiron <mbaleand@myuax.com>
2024-10-15 16:53:07 +0200

    Libreria Matematicas.cpp añadida

--- Ejercicios_3_7/main.cpp
+++ Ejercicios_3_7/main.cpp
@@ -2,6 +2,7 @@
 #include <ostream>
 #include <fmt/core.h>
 #include "Funciones.h"
+#include "Matematicas.cpp"
 
 int main() {
     fmt::print("Hello, {}!\n", "world");
@@ -14,6 +15,9 @@ int main() {
 
     int resultado_multi = multi(2, 8);
     std::cout << "La multiplicacion es: " << resultado_multi << std::endl;
+
+    int resultado_divi = divi(8, 2);
+    std::cout << "La division es: " << resultado_divi << std::endl;
 
     return 0;
 }
